2efe5f32afff5c03b03f54e35660c012d2f5cf7e
Jenny Branham <branhamjenny@gmail.com>
2021-02-21 14:08:01 -0600

    Add models and tests

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,8 @@
 class Machine < ApplicationRecord
   validates_presence_of :location
-  
+  validates_uniqueness_of :location
+  has_many :machine_snacks
+  has_many :snacks, through: :machine_snacks
   belongs_to :owner
+
 end
